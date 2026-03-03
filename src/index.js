1829577fc84222ea0bd641e78981a0ea5affd48f
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2026-03-02 17:23:41 -0800

    Refactor task-mgr
    taskList is now the tasks object, which
    contains the task list, categories list, and
    method to add new tasks.
    Currently wrestling with date functions...
    
    Next commit:
    -straighten out date handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import { tasks } from "./task-mgr.js";
+
+console.clear();
 console.log("Howdy partner.")
+console.log(tasks);
+tasks.add("laundry", "chores", "low", "3/1/26", "towels and sheets");
+console.log(tasks);
+tasks.list[0].edit("priority", "high");
+tasks.list[0].checkDue();
+console.log(tasks.list[0]);
--- src/task-mgr.js
+++ src/task-mgr.js
@@ -1,32 +1,41 @@
-// Need to read up on date-fns and cherry pick, for (at least) checkDueDate()
-// import { } from "date-fns";
+// Need to read up on date-fns and cherry pick, for (at least) checkDue()
+import { format, isAfter } from "date-fns";
 
-const taskList = [];
+const formattedDate = (date) => format(new Date(date), 'PPPP');
+
+const tasks = {
+    list: [],
+    categories: [],
+    add(name, ...rest) {
+        const [category, priority, due, details] = rest;
+        if (!this.categories.includes(category)) {
+            this.categories.push(category);
+        };
+        const task = this.list
+                        .push(new Task(name, category, priority, due, details));
+    },
+};
 
 class Task {
-    constructor(name, category="default", priority="low", dueDate=undefined, details=undefined) {
+    constructor(name, category="general", priority="low", due=undefined, details=undefined) {
         this.name = name;
         this.category = category;
         this.priority = priority;
-        this.dueDate = dueDate;
-        this.status = "yet to do";
+        this.due = due === undefined ? due : formattedDate(due);
         this.details = details;
     }
 
+    status = "yet to do";
+
     edit(property, newValue) {
-        this[property] = newValue;
+        this[property] = property === "due" ? 
+                        formattedDate(newValue) : newValue;
     }
 
-    checkDueDate() {
-        const currDate = new Date();
-        if (currDate > this.dueDate) this.status = "overdue";
+    checkDue() {
+        const currDate = format(new Date(), 'PPPP');
+        console.log(isAfter(currDate, this.due));
     }
 }
 
-function createTask(name, ...rest) {
-    const [category, priority, dueDate, details] = rest;
-    const task = new Task(name, category, priority, dueDate, details);
-    taskList.push(task);
-}
-
-export { taskList, createTask };
+export { tasks };
