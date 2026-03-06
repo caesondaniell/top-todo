3fb7d60cef007a6bf02a259d35205484600cb015
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2026-03-06 14:27:55 -0800

    Add and style menu button
    Also linked Material Symbols to template.html
    
    Future commits:
    -feature: add/edit/delete categories and tabs
    -feature: task functionality (add, edit, etc.)
    -feature: view/interact with closed tasks

--- src/dom-control.js
+++ src/dom-control.js
@@ -2,9 +2,11 @@ import { tasks } from "./task-mgr.js";
 
 let displayedList = tasks.open;
 const createDiv = createElement("div");
+const createModal = createElement("dialog");
 const createH1 = createElement("h1");
 const createH3 = createElement("h3");
 const createPara = createElement("p");
+const createSpan = createElement("span");
 const createButton = createElement("button");
 const createLink = createElement("a");
 
@@ -63,18 +65,26 @@ function renderTasks() {
 
 export function renderPage() {
     const main = createDiv();
-    const pageTitle = createH1();
+    const head = createDiv();
     const tabs = createDiv();
-    const allTab = createButton();
     const listDisplay = createDiv();
+    const pageTitle = createH1();
+    const menu = createButton();
+    const menuIcon = createSpan();
+    const allTab = createButton();
+    
     main.classList.add("main");
+    head.classList.add("head");
+    menuIcon.classList.add("material-symbols-outlined")
+    menu.classList.add("menu");
     tabs.classList.add("tabs");
     allTab.classList.add("list-tab");
     allTab.classList.add("active");
     listDisplay.classList.add("list-display");
     allTab.setAttribute("data-category", "all");
     pageTitle.textContent = "My To-Dos";
-    allTab.textContent = "all tasks"
+    menuIcon.textContent = "more_vert";
+    allTab.textContent = "all tasks";
     tabs.addEventListener("click", (e) => {
         const btn = e.target.closest(".list-tab");
         const category = btn.dataset.category;
@@ -88,8 +98,11 @@ export function renderPage() {
         }
         renderTasks();
     });
+    menu.appendChild(menuIcon);
+    head.appendChild(pageTitle);
+    head.appendChild(menu);
     tabs.appendChild(allTab);
-    main.appendChild(pageTitle);
+    main.appendChild(head);
     main.appendChild(tabs);
     main.appendChild(listDisplay);
     document.body.appendChild(main);
