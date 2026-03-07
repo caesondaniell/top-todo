50a99015368b4bcd364f47c27ec430e099c1ff32
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2026-03-06 16:33:39 -0800

    Add feature: options menu
    Menu opens when the options button is clicked.
    Working toward task, category, and archive
    features.
    
    Next commit:
    -fix bug: tabs event listener
    
    Future commits:
    -Continued work on:
    --add/edit/delete categories
    --add tasks
    --view archive

--- src/dom-control.js
+++ src/dom-control.js
@@ -1,19 +1,51 @@
 import { tasks } from "./task-mgr.js";
 
 let displayedList = tasks.open;
+
+const createButton = createElement("button");
 const createDiv = createElement("div");
-const createModal = createElement("dialog");
 const createH1 = createElement("h1");
 const createH3 = createElement("h3");
+const createLink = createElement("a");
+const createList = createElement("ul");
+const createListItem = createElement("li");
+const createModal = createElement("dialog");
 const createPara = createElement("p");
 const createSpan = createElement("span");
-const createButton = createElement("button");
-const createLink = createElement("a");
+
+const optionsMenu = (() => {
+    const container = createModal();
+    const newTask = createOption("assignment_add", "New Task");
+    const editCategories = createOption("edit_square", "Edit Categories");
+    const viewArchive = createOption("folder", "View Closed Tasks");
+    container.classList.add("options");
+    container.appendChild(newTask);
+    container.appendChild(editCategories);
+    container.appendChild(viewArchive);
+    document.body.appendChild(container);
+})()
 
 function createElement(element) {
     return () => {
         return document.createElement(element);
     }
+}
+
+function createIcon() {
+    const icon = createSpan();
+    icon.classList.add("material-symbols-outlined");
+    icon.toggleAttribute("aria-hidden");
+    return icon;
+}
+
+function createOption(iconName, label) {
+    const p = createPara();
+    const icon = createIcon();
+    icon.textContent = iconName;
+
+    p.textContent = ` ${label}`;
+    p.prepend(icon);
+    return p;
 }
 
 function renderTabs() {
@@ -69,22 +101,22 @@ export function renderPage() {
     const tabs = createDiv();
     const listDisplay = createDiv();
     const pageTitle = createH1();
-    const menu = createButton();
-    const menuIcon = createSpan();
+    const options = createButton();
+    const optionsIcon = createIcon();
     const allTab = createButton();
-    
     main.classList.add("main");
     head.classList.add("head");
-    menuIcon.classList.add("material-symbols-outlined")
-    menu.classList.add("menu");
+    options.classList.add("options-menu");
     tabs.classList.add("tabs");
     allTab.classList.add("list-tab");
     allTab.classList.add("active");
     listDisplay.classList.add("list-display");
+    options.setAttribute("aria-label", "list options");
     allTab.setAttribute("data-category", "all");
     pageTitle.textContent = "My To-Dos";
-    menuIcon.textContent = "more_vert";
+    optionsIcon.textContent = "more_vert";
     allTab.textContent = "all tasks";
+    optionsIcon.addEventListener("click", toggleOptions);
     tabs.addEventListener("click", (e) => {
         const btn = e.target.closest(".list-tab");
         const category = btn.dataset.category;
@@ -98,9 +130,9 @@ export function renderPage() {
         }
         renderTasks();
     });
-    menu.appendChild(menuIcon);
+    options.appendChild(optionsIcon);
     head.appendChild(pageTitle);
-    head.appendChild(menu);
+    head.appendChild(options);
     tabs.appendChild(allTab);
     main.appendChild(head);
     main.appendChild(tabs);
@@ -108,4 +140,29 @@ export function renderPage() {
     document.body.appendChild(main);
     renderTabs();
     renderTasks();
+}
+
+function handleOutsideClick(e) {
+    const options = document.querySelector(".options");
+    const optionsButton = document.querySelector(".options-menu");
+
+    if (!options.contains(e.target) && !optionsButton.contains(e.target)) {
+        toggleOptions();
+    };
+}
+
+function toggleOptions() {
+    const options = document.querySelector(".options");
+    switch (options.hasAttribute("open")) {
+        case true:
+            options.close();
+            document.removeEventListener("click", handleOutsideClick);
+            break;
+        case false:
+            options.show();
+            setTimeout(() => {
+                document.addEventListener("click", handleOutsideClick);
+            }, 0);
+            break;
+    };
 }
